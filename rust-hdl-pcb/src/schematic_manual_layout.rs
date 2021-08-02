1e6a24e382e49fd117e592e322af02ff391ac398
sbasu <samit.basu@smiths.com>
2021-08-01 21:00:11 -0700

    Fixup manual layout.

--- rust-hdl-pcb/src/schematic_manual_layout.rs
+++ rust-hdl-pcb/src/schematic_manual_layout.rs
@@ -9,7 +9,7 @@ use crate::circuit::SchematicRotation::{Horizontal, Vertical};
 
 pub fn place(x: CircuitNode, xc: i32, yc: i32, orientation: SchematicRotation) -> PartInstance {
     let mut y: PartInstance = x.into();
-    if rotated {
+    if orientation == SchematicRotation::Vertical {
         y = y.rot90();
     }
     y.schematic_orientation.center = crate::glyph::Point {
@@ -22,9 +22,9 @@ pub fn place(x: CircuitNode, xc: i32, yc: i32, orientation: SchematicRotation) -
 #[test]
 fn test_manual_layout() {
     let in_resistor = place(make_yageo_series_resistor("RC1206FR-071KL"), 0, 200, Horizontal);
-    let input_cap = place(make_murata_capacitor("GRT188R61H105KE13D"), 700, -150, Vertical);
-    let v_reg = place(make_ti_tps_7b84_regulator("TPS7B8433QDCYRQ1"), 1900, 0, Horizontal);
-    let output_cap = place(make_yageo_cc_series_cap("CC0805KKX5R8BB106"), 3000, -150, Vertical);
+    let input_cap = place(make_murata_capacitor("GRT188R61H105KE13D"), 900, -150, Vertical);
+    let v_reg = place(make_ti_tps_7b84_regulator("TPS7B8433QDCYRQ1"), 2300, 0, Horizontal);
+    let output_cap = place(make_yageo_cc_series_cap("CC0805KKX5R8BB106"), 3500, -150, Vertical);
     let circuit = vec![&in_resistor, &input_cap, &v_reg, &output_cap];
     write_circuit_to_svg(&circuit, "test_circuit_manual.svg");
 }
