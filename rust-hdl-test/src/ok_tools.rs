37ff70de9301da842862a9b4469ade6576fe736e
Samit Basu <samit.basu@smiths.com>
2021-09-10 11:09:06 -0700

    One more attempt at collision detection...

--- rust-hdl-test/src/ok_tools.rs
+++ rust-hdl-test/src/ok_tools.rs
@@ -13,9 +13,9 @@ const MIG_DIR: &str = "/opt/FrontPanel-Ubuntu16.04LTS-x64-5.2.0/Samples/RAMTeste
 pub fn find_ok_bus_collisions(vlog: &str) {
     let expr = regex::Regex::new(r#"\.ep_addr\(8'h(\w+)\)"#).unwrap();
     let mut addr_list = vec![];
-    for capture in expr.captures(vlog) {
+    for capture in expr.captures_iter(vlog) {
         let port = capture.get(1).unwrap().as_str();
-        assert!(!addr_list.contains(&port.to_string()));
+        assert!(!addr_list.contains(&port.to_string()), "Found duplicate port! {}", port);
         addr_list.push(port.to_owned());
     }
 }
