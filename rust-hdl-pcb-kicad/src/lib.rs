5635acdc2fafa4cb77ec11a7c16f0bc62c989ef6
sbasu <samit.basu@smiths.com>
2021-10-01 15:11:07 -0700

    Map glyphs from RustPCB to KiCAD.

--- rust-hdl-pcb-kicad/src/lib.rs
+++ rust-hdl-pcb-kicad/src/lib.rs
@@ -1,15 +1,18 @@
 #![allow(non_camel_case_types)]
 
+use rust_hdl_pcb_core::prelude::*;
 use std::fmt::Display;
 use std::str::FromStr;
+
+use serde::{ser, Serialize};
+use uuid::Uuid;
 
 use crate::glyph::{polyline, rectangle};
 use crate::pins::pin;
 use crate::serde_s::to_s_string;
-use serde::{ser, Serialize};
-use uuid::Uuid;
 
 pub mod serde_s;
+mod test;
 
 #[derive(Debug, Clone, Serialize)]
 pub enum FontDetail {
@@ -21,6 +24,7 @@ pub enum Justification {
     right,
     left,
     bottom,
+    top,
 }
 
 #[derive(Debug, Clone, Serialize)]
@@ -99,6 +103,14 @@ fn make_font_size(sze: f64) -> Effects {
 }
 
 #[derive(Debug, Clone, Serialize)]
+#[serde(rename = "radius")]
+pub struct ArcDetails {
+    at: (f64, f64),
+    length: f64,
+    angles: (f64, f64),
+}
+
+#[derive(Debug, Clone, Serialize)]
 pub enum glyph {
     rectangle {
         start: (f64, f64),
@@ -107,6 +119,22 @@ pub enum glyph {
     },
     polyline {
         pts: Vec<xy>,
+        _visuals: Vec<Visual>,
+    },
+    text {
+        _text: String,
+        at: (f64, f64, f64),
+        _effects: Effects,
+    },
+    circle {
+        center: (f64, f64),
+        radius: f64,
+        _visuals: Vec<Visual>,
+    },
+    arc {
+        start: (f64, f64),
+        end: (f64, f64),
+        _details: ArcDetails,
         _visuals: Vec<Visual>,
     },
 }
@@ -152,7 +180,8 @@ pub struct property {
 }
 
 #[derive(Debug, Clone, Serialize)]
-pub struct symbol {
+#[serde(rename = "symbol")]
+pub struct LibrarySymbol {
     _name: String,
     in_bom: bool,
     on_board: bool,
@@ -170,7 +199,7 @@ pub struct Page {
 
 #[derive(Debug, Clone, Serialize)]
 pub enum Element {
-    lib_symbols(Vec<symbol>),
+    lib_symbols(Vec<LibrarySymbol>),
     junction {
         at: (f64, f64),
         diameter: f64,
@@ -224,164 +253,109 @@ pub struct KiCadSchematic {
     _elements: Vec<Element>,
 }
 
-#[test]
-fn test_schematic() {
-    let y = KiCadSchematic {
+fn mils_to_mm(x: i32) -> f64 {
+    fmils_to_mm(f64::from(x))
+}
+fn fmils_to_mm(x: f64) -> f64 {
+    f64::from(x) / 1000.0 * 25.4
+}
+
+fn map_glyphs_to_shapes(glyphs: &[Glyph], hide_outline: bool) -> Vec<glyph> {
+    let mut ret = vec![];
+    for x in &glyphs {
+        match x {
+            Glyph::OutlineRect(r) => {
+                if !hide_outline {
+                    ret.push(glyph::rectangle {
+                        start: (mils_to_mm(r.p0.x), -mils_to_mm(-r.p0.y)),
+                        end: (mils_to_mm(r.p1.x), -mils_to_mm(-r.p1.y)),
+                        _visuals: vec![
+                            Visual::stroke(crate::make_stroke_width(0.254)),
+                            Visual::fill(Fill(FillType::background)),
+                        ],
+                    });
+                }
+            }
+            Glyph::Line(l) => ret.push(glyph::polyline {
+                pts: vec![
+                    xy(mils_to_mm(l.p0.x), mils_to_mm(-l.p0.y)),
+                    xy(mils_to_mm(l.p1.x), mils_to_mm(-l.p1.y)),
+                ],
+                _visuals: vec![
+                    Visual::stroke(crate::make_stroke_width(0.0)),
+                    Visual::fill(Fill(FillType::none)),
+                ],
+            }),
+            Glyph::Text(t) => ret.push(glyph::text {
+                _text: t.text.clone(),
+                at: (mils_to_mm(t.p0.x), mils_to_mm(-t.p1.y), 0.0),
+                _effects: Effects(vec![
+                    Effect::font(vec![FontDetail::size(1.27, 1.27)]),
+                    Effect::justify(match t.justify {
+                        TextJustification::BottomLeft => {
+                            vec![Justification::bottom, Justification::left]
+                        }
+                        TextJustification::BottomRight => {
+                            vec![Justification::bottom, Justification::right]
+                        }
+                        TextJustification::TopLeft => {
+                            vec![Justification::top, Justification::left]
+                        }
+                        TextJustification::TopRight => {
+                            vec![Justification::top, Justification::right]
+                        }
+                        TextJustification::MiddleLeft => {
+                            vec![Justification::left]
+                        }
+                        TextJustification::MiddleRight => {
+                            vec![Justification::right]
+                        }
+                    }),
+                ]),
+            }),
+            Glyph::Arc(_) => {}
+            Glyph::Circle(c) => glyph::circle {
+                center: (mils_to_mm(c.p0.x), mils_to_mm(-c.p0.y)),
+                radius: fmils_to_mm(c.radius),
+                _visuals: vec![
+                    Visual::stroke(crate::make_stroke_width(0.0)),
+                    Visual::fill(Fill(FillType::none)),
+                ],
+            },
+        }
+    }
+    ret
+}
+
+fn map_part_to_library_symbols(part: &PartDetails) -> LibrarySymbol {
+    LibrarySymbol {
+        _name: part.label.to_string(),
+        in_bom: true,
+        on_board: true,
+        _properties: vec![ // TODO - add these later
+        ],
+        _shape: shape {
+            _name: part.label.to_string(),
+            _elements: map_glyphs_to_shapes(&part.outline, part.hide_part_outline),
+        },
+        _pinout: pinout {
+            _name: part.label.to_string(),
+            _elements: vec![],
+        },
+    }
+    // The first symbol represents the
+}
+
+pub fn write_circuit_to_kicad6(circuit: &Circuit, layout: &SchematicLayout, name: &str) {
+    let mut schematic = KiCadSchematic {
         version: 20210621,
         generator: Generator::eeschema,
-        uuid: Uuid::from_str("2a3d3e67-16d3-407f-b629-075136792054").unwrap(),
+        uuid: Uuid::new_v4(),
         paper: "A4".to_string(),
-        _elements: vec![
-            Element::lib_symbols(vec![symbol {
-                _name: "Converter_DCDC:TMR2-2411WI".to_string(),
-                in_bom: true,
-                on_board: true,
-                _properties: vec![
-                    property {
-                        _name: "Reference".to_string(),
-                        _value: "U".to_string(),
-                        id: 0,
-                        at: (-7.62, 8.89, 0.0),
-                        effects: make_font_size(1.27).0,
-                    },
-                    property {
-                        _name: "Value".to_string(),
-                        _value: "TMR2-2411WI".to_string(),
-                        id: 1,
-                        at: (11.43, 8.89, 0.0),
-                        effects: {
-                            let mut r = make_font_size(1.27).0;
-                            r.push(Effect::justify(vec![Justification::right]));
-                            r
-                        },
-                    },
-                ],
-                _shape: shape {
-                    _name: "TMR2-2411WI_0_1".to_string(),
-                    _elements: vec![
-                        rectangle {
-                            start: (-10.16, 7.62),
-                            end: (10.16, -7.62),
-                            _visuals: vec![
-                                Visual::stroke(make_stroke_width(0.254)),
-                                Visual::fill(Fill(FillType::background)),
-                            ],
-                        },
-                        polyline {
-                            pts: vec![xy(0.0, -5.08), xy(0.0, -6.35)],
-                            _visuals: vec![
-                                Visual::stroke(make_stroke_width(0.0)),
-                                Visual::fill(Fill(FillType::none)),
-                            ],
-                        },
-                    ],
-                },
-                _pinout: pinout {
-                    _name: "TMR2-2411WI_1_1".to_string(),
-                    _elements: vec![
-                        pin {
-                            _kind: PinKind::power_in,
-                            _appears: PinAppearance::line,
-                            at: (-12.7, -5.08, 0.0),
-                            length: 2.54,
-                            _hide: None,
-                            name: ("-VIN".to_string(), make_font_size(1.27)),
-                            number: ("1".to_string(), make_font_size(1.27)),
-                        },
-                        pin {
-                            _kind: PinKind::power_in,
-                            _appears: PinAppearance::line,
-                            at: (-12.7, 5.08, 0.0),
-                            length: 2.54,
-                            _hide: None,
-                            name: ("+VIN".to_string(), make_font_size(1.27)),
-                            number: ("2".to_string(), make_font_size(1.27)),
-                        },
-                        pin {
-                            _kind: PinKind::no_connect,
-                            _appears: PinAppearance::line,
-                            at: (10.16, 2.54, 180.0),
-                            length: 2.54,
-                            _hide: Some(PinHide::hide),
-                            name: ("NC".to_string(), make_font_size(1.27)),
-                            number: ("7".to_string(), make_font_size(1.27)),
-                        },
-                    ],
-                },
-            }]),
-            Element::junction {
-                at: (124.46, 57.15),
-                diameter: 0.9144,
-                color: (0.0, 0.0, 0.0, 0.0),
-            },
-            Element::junction {
-                at: (133.35, 74.93),
-                diameter: 0.0,
-                color: (0.0, 0.0, 0.0, 0.0),
-            },
-            Element::wire {
-                pts: vec![xy(105.41, 59.69), xy(113.03, 59.68)],
-                stroke: vec![
-                    StrokeDetails::width(0.0),
-                    StrokeDetails::kind(StrokeKind::solid),
-                    StrokeDetails::color(0.0, 0.0, 0.0, 0.0),
-                ],
-                uuid: Uuid::from_str("6222b708-5a6d-4f91-8f28-6abbc1ebfea0").unwrap(),
-            },
-            Element::symbol {
-                lib_id: "Converter_DCDC:TMR2-2411WI".to_string(),
-                at: (92.71, 64.77, 0.0),
-                unit: 1,
-                in_bom: true,
-                on_board: true,
-                _auto: Some(AutoFields {}),
-                uuid: Uuid::from_str("33abe0c8-3486-4c7c-a2ef-f55fc4bed422").unwrap(),
-                _properties: vec![property {
-                    _name: "Reference".into(),
-                    _value: "U1".into(),
-                    id: 0,
-                    at: (92.71, 52.07, 0.0),
-                    effects: vec![],
-                }],
-                _pin: vec![
-                    PinMap {
-                        _number: "1".into(),
-                        uuid: Uuid::from_str("6a5a1852-4c63-44e1-b56f-75954e493785").unwrap(),
-                    },
-                    PinMap {
-                        _number: "2".into(),
-                        uuid: Uuid::from_str("46a44479-2cb4-428c-8a60-e5bccebe2cab").unwrap(),
-                    },
-                    PinMap {
-                        _number: "3".into(),
-                        uuid: Uuid::from_str("593540a1-4bce-40ce-bd4b-b0b5c80ae860").unwrap(),
-                    },
-                    PinMap {
-                        _number: "6".into(),
-                        uuid: Uuid::from_str("ed5b2a22-eee0-4877-87ea-c441ec704706").unwrap(),
-                    },
-                    PinMap {
-                        _number: "7".into(),
-                        uuid: Uuid::from_str("e39a3e9e-c9fc-4b7f-88b9-cce9f4b1d655").unwrap(),
-                    },
-                    PinMap {
-                        _number: "8".into(),
-                        uuid: Uuid::from_str("3d3be133-493f-452c-a56b-f68961872065").unwrap(),
-                    },
-                    PinMap {
-                        _number: "9".into(),
-                        uuid: Uuid::from_str("9dffd0c4-c608-4a1d-9b31-9b886dcb2a90").unwrap(),
-                    },
-                ],
-            },
-            Element::sheet_instances {
-                _pages: vec![Page {
-                    _path: "/".to_string(),
-                    page: "1".to_string(),
-                }],
-            },
-        ],
+        _elements: vec![],
     };
-    let q = to_s_string(&y).unwrap();
-    println!("{}", q);
+    for part in &circuit.nodes {
+        let part = get_details_from_instance(part, layout);
+        let symbols = map_part_to_library_symbols(&part);
+    }
 }
--- rust-hdl-pcb-svg/src/schematic.rs
+++ rust-hdl-pcb-svg/src/schematic.rs
@@ -321,54 +321,6 @@ pub fn add_outline_to_path(doc: Group, g: &Glyph, hide_outline: bool) -> Group {
     }
 }
 
-pub fn make_flip_lr_part(part: &PartDetails) -> PartDetails {
-    let mut fpart = part.clone();
-    fpart.outline = part.outline.iter().map(|x| x.fliplr()).collect();
-    fpart.pins = part
-        .pins
-        .iter()
-        .map(|x| (*x.0, EPin::new(&x.1.name, x.1.kind, x.1.location.fliplr())))
-        .collect();
-    fpart
-}
-
-pub fn make_flip_ud_part(part: &PartDetails) -> PartDetails {
-    let mut fpart = part.clone();
-    fpart.outline = part.outline.iter().map(|x| x.flipud()).collect();
-    fpart.pins = part
-        .pins
-        .iter()
-        .map(|x| (*x.0, EPin::new(&x.1.name, x.1.kind, x.1.location.flipud())))
-        .collect();
-    fpart
-}
-
-pub fn get_details_from_instance(x: &PartInstance, l: &SchematicLayout) -> PartDetails {
-    let mut part = match &x.node {
-        CircuitNode::Capacitor(c) => &c.details,
-        CircuitNode::Resistor(r) => &r.details,
-        CircuitNode::Diode(d) => &d.details,
-        CircuitNode::Regulator(v) => &v.details,
-        CircuitNode::Inductor(l) => &l.details,
-        CircuitNode::IntegratedCircuit(u) => u,
-        CircuitNode::Connector(j) => j,
-        CircuitNode::Logic(u) => &u.details,
-        CircuitNode::Port(p) => p,
-    }
-    .clone();
-
-    let layout = l.part(&x.id);
-    if layout.flipped_lr {
-        part = make_flip_lr_part(&part);
-    }
-
-    if layout.flipped_ud {
-        part = make_flip_ud_part(&part);
-    }
-
-    part
-}
-
 fn make_rect_into_data(r: &Rect) -> Data {
     Data::new()
         .move_to((r.p0.x, -r.p0.y))
