33ce08687c2cb48b9f5291beb2c622547395341c
Samit Basu <samit.basu@smiths.com>
2021-10-22 09:05:40 -0700

    More work for the top level crate.

--- rust-hdl/src/lib.rs
+++ rust-hdl/src/lib.rs
@@ -1,12 +1,27 @@
-pub use rust_hdl_core::prelude::*;
-pub use rust_hdl_widgets::prelude::*;
-pub use rust_hdl_yosys_synth::*;
+pub use rust_hdl_core as core;
+pub use rust_hdl_widgets as widgets;
+pub use rust_hdl_yosys_synth as yosys_synth;
 #[cfg(feature = "ok")]
-pub use rust_hdl_bsp_ok_xem6010;
+pub use rust_hdl_bsp_ok_xem6010 as bsp_ok_xem6010;
 #[cfg(feature = "ok")]
-pub use rust_hdl_bsp_ok_xem7010;
+pub use rust_hdl_bsp_ok_xem7010 as bsp_ok_xem7010;
 #[cfg(feature = "sim")]
-pub use rust_hdl_sim_chips;
+pub use rust_hdl_sim_chips as sim_chips;
+#[cfg(feature = "alchitry_cu")]
+pub use rust_hdl_bsp_alchitry_cu as bsp_alchitry_cu;
+#[cfg(feature = "ok")]
+pub use rust_hdl_ok_core as ok_core;
+#[cfg(feature = "ok")]
+pub use rust_hdl_ok_frontpanel_sys as ok_frontpanel_sys;
+#[cfg(feature = "test_tools")]
+pub use rust_hdl_test_core as test_core;
+#[cfg(all(feature = "test_tools", feature = "ok"))]
+pub use rust_hdl_test_ok_common as test_ok_common;
+#[cfg(feature = "toolchains")]
+pub use rust_hdl_toolchain_common as toolchain_common;
+#[cfg(feature = "toolchains")]
+pub use rust_hdl_toolchain_icestorm as toolchain_icestorm;
+
 
 
 
