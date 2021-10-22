1a5c8530316e2d5c46081ee173b1806ba8480eff
Samit Basu <samit.basu@smiths.com>
2021-10-22 09:15:25 -0700

    More updates to the top level cargo file.

--- rust-hdl-sim-chips/src/prelude.rs
+++ rust-hdl-sim-chips/src/prelude.rs
@@ -1,6 +1,6 @@
 pub use crate::ad7193_sim::*;
 pub use crate::ads868x_sim::*;
 pub use crate::max31856_sim::*;
+pub use crate::max31856_sim::*;
 pub use crate::muxed_ad7193_sim::*;
 pub use crate::muxed_ads868x_sim::*;
-pub use crate::max31856_sim::*;
--- rust-hdl/src/lib.rs
+++ rust-hdl/src/lib.rs
@@ -1,27 +1,25 @@
-pub use rust_hdl_core as core;
-pub use rust_hdl_widgets as widgets;
-pub use rust_hdl_yosys_synth as yosys_synth;
+#[cfg(feature = "alchitry_cu")]
+pub use rust_hdl_bsp_alchitry_cu as bsp_alchitry_cu;
 #[cfg(feature = "ok")]
 pub use rust_hdl_bsp_ok_xem6010 as bsp_ok_xem6010;
 #[cfg(feature = "ok")]
 pub use rust_hdl_bsp_ok_xem7010 as bsp_ok_xem7010;
-#[cfg(feature = "sim")]
-pub use rust_hdl_sim_chips as sim_chips;
-#[cfg(feature = "alchitry_cu")]
-pub use rust_hdl_bsp_alchitry_cu as bsp_alchitry_cu;
+pub use rust_hdl_core as core;
 #[cfg(feature = "ok")]
 pub use rust_hdl_ok_core as ok_core;
 #[cfg(feature = "ok")]
 pub use rust_hdl_ok_frontpanel_sys as ok_frontpanel_sys;
+#[cfg(feature = "sim")]
+pub use rust_hdl_sim_chips as sim_chips;
 #[cfg(feature = "test_tools")]
 pub use rust_hdl_test_core as test_core;
 #[cfg(all(feature = "test_tools", feature = "ok"))]
 pub use rust_hdl_test_ok_common as test_ok_common;
-#[cfg(feature = "toolchains")]
-pub use rust_hdl_toolchain_common as toolchain_common;
-#[cfg(feature = "toolchains")]
+#[cfg(feature = "icestorm")]
 pub use rust_hdl_toolchain_icestorm as toolchain_icestorm;
-
-
-
-
+#[cfg(feature = "ise")]
+pub use rust_hdl_toolchain_ise as toolchain_ise;
+#[cfg(feature = "vivado")]
+pub use rust_hdl_toolchain_vivado as toolchain_vivado;
+pub use rust_hdl_widgets as widgets;
+pub use rust_hdl_yosys_synth as yosys_synth;
