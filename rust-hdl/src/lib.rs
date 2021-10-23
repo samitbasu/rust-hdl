c4132b45f3995924b178f3c26a823360b491c343
Samit Basu <samit.basu@smiths.com>
2021-10-22 17:41:22 -0700

    Re-introduce frontpanel dep.

--- rust-hdl/src/lib.rs
+++ rust-hdl/src/lib.rs
@@ -7,6 +7,8 @@ pub use rust_hdl_bsp_ok_xem7010 as bsp_ok_xem7010;
 pub use rust_hdl_core as core;
 #[cfg(feature = "ok")]
 pub use rust_hdl_ok_core as ok_core;
+#[cfg(feature = "ok")]
+pub use rust_hdl_ok_frontpanel_sys as ok_frontpanel_sys;
 #[cfg(feature = "sim")]
 pub use rust_hdl_sim_chips as sim_chips;
 #[cfg(feature = "test_tools")]
