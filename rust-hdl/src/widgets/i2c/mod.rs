baa036eb9d5ca60c6bce1a66d90223c7ac88052e
Samit Basu <samit.basu@smiths.com>
2022-02-03 16:43:55 -0800

    Added read and write.  Fixed some issues.  Added random read/write test harness with 2 simulated devices.  Seems OK.

--- rust-hdl/src/widgets/i2c/mod.rs
+++ rust-hdl/src/widgets/i2c/mod.rs
@@ -2,3 +2,4 @@ pub mod i2c_controller;
 pub mod i2c_driver;
 pub mod i2c_target;
 pub mod i2c_test_target;
+pub mod sim;
---
+++ rust-hdl/src/widgets/i2c/sim.rs
@@ -0,0 +1,68 @@
+#[macro_export]
+macro_rules! i2c_begin_write {
+    ($sim: ident, $clock: ident, $uut: ident, $addr: expr) => {
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_true!($sim, $clock, $uut);
+        $uut.controller.cmd.next = I2CControllerCmd::BeginWrite;
+        $uut.controller.write_data_in.next = $addr.into();
+        $uut.controller.run.next = true;
+        wait_clock_cycle!($sim, $clock, $uut);
+        $uut.controller.run.next = false;
+        $uut = $sim.watch(|x| x.controller.nack.val() | x.controller.ack.val(), $uut)?;
+    };
+}
+
+#[macro_export]
+macro_rules! i2c_begin_read {
+    ($sim: ident, $clock: ident, $uut: ident, $addr: expr) => {
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_true!($sim, $clock, $uut);
+        $uut.controller.cmd.next = I2CControllerCmd::BeginRead;
+        $uut.controller.write_data_in.next = $addr.into();
+        $uut.controller.run.next = true;
+        wait_clock_cycle!($sim, $clock, $uut);
+        $uut.controller.run.next = false;
+        $uut = $sim.watch(|x| x.controller.nack.val() | x.controller.ack.val(), $uut)?;
+    };
+}
+
+#[macro_export]
+macro_rules! i2c_end_transmission {
+    ($sim: ident, $clock: ident, $uut: ident) => {
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_true!($sim, $clock, $uut);
+        $uut.controller.cmd.next = I2CControllerCmd::EndTransmission;
+        $uut.controller.run.next = true;
+        wait_clock_cycle!($sim, $clock, $uut);
+        $uut.controller.run.next = false;
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_cycles!($sim, $clock, $uut, 10);
+    };
+}
+
+#[macro_export]
+macro_rules! i2c_write {
+    ($sim: ident, $clock: ident, $uut: ident, $val: expr) => {
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_true!($sim, $clock, $uut);
+        $uut.controller.cmd.next = I2CControllerCmd::Write;
+        $uut.controller.write_data_in.next = $val.into();
+        $uut.controller.run.next = true;
+        wait_clock_cycle!($sim, $clock, $uut);
+        $uut.controller.run.next = false;
+        $uut = $sim.watch(|x| x.controller.nack.val() | x.controller.ack.val(), $uut)?;
+    };
+}
+#[macro_export]
+macro_rules! i2c_read {
+    ($sim: ident, $clock: ident, $uut: ident) => {{
+        $uut = $sim.watch(|x| !x.controller.busy.val(), $uut)?;
+        wait_clock_true!($sim, $clock, $uut);
+        $uut.controller.cmd.next = I2CControllerCmd::Read;
+        $uut.controller.run.next = true;
+        wait_clock_cycle!($sim, $clock, $uut);
+        $uut.controller.run.next = false;
+        $uut = $sim.watch(|x| x.controller.read_valid.val(), $uut)?;
+        $uut.controller.read_data_out.val()
+    }};
+}
