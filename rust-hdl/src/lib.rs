108c173a3d2fd02a1e5429b9afb8268cf2b9d0df
sbasu <samit.basu@smiths.com>
2022-09-01 15:17:45 -0700

    Add verilog generation.

--- rust-hdl/src/lib.rs
+++ rust-hdl/src/lib.rs
@@ -1042,14 +1042,80 @@
 //! impl Logic for Foo {
 //!    #[hdl_gen]
 //!    fn update(&mut self) {
-//!       self.consumer.data_to_fifo.next = self.producer.data_to_fifo.val();
-//!       self.consumer.write.next = self.producer.write.val();
-//!       self.producer.full.next = self.consumer.full.val();
-//!       self.producer.overflow.next = self.consumer.overflow.val();
+//!       match self.sig_in.val() {
+//!           State1::On => self.sig_out.next = State2::On,
+//!           State1::Off => self.sig_out.next = State2::Off,
+//!       }
 //!    }
 //! }
 //! ```
 //!
+//! ## Interfaces
+//!
+//! One area you will encouter as your circuits become more complex is that the interfaces
+//! to those circuits will become increasingly complicated.  To demonstrate, suppose you
+//! have a circuit that consumes a sequence of 16-bit integers via a FIFO interface.  The
+//! circuit has some flow control signals because it cannot consume them every clock
+//! cycle (For Reasons).  Suppose also that you have a data producer circuit that will
+//! produce 16-bit integers and you want to connect these two together.  A natural
+//! FIFO interface would look like this
+//!
+//! ```rust
+//!# use rust_hdl::prelude::*;
+//!  struct MyFIFO {
+//!      pub data_to_fifo: Signal<In, Bits<16>>,
+//!      pub write: Signal<In, Bits<16>>,
+//!      pub full: Signal<Out, Bit>,
+//!      pub overflow: Signal<Out, Bit>,
+//!  }
+//!
+//!  struct DataWidget {
+//!      pub data_to_fifo: Signal<Out, Bits<16>>,
+//!      pub write: Signal<Out, Bits<16>>,
+//!      pub full: Signal<In, Bit>,
+//!      pub overflow: Signal<In, Bit>,
+//!  }
+//!
+//!  struct Foo {
+//!     producer: DataWidget,
+//!     consumer: MyFIFO,
+//!  }
+//! ```
+//!
+//! Now, we want to connect the output of the DataWidget (all 4 signals!) to the corresponding
+//! signals on `MyFIFO`.  Keep in mind that the order of assignment is irrelevant, but which
+//! signal appears on the LHS vs RHS _is_ important.  In the `impl Logic` block for `Foo`,
+//! our HDL kernel will look like this:
+//!```rust
+//!# use rust_hdl::prelude::*;
+//!#  struct MyFIFO {
+//!#      pub data_to_fifo: Signal<In, Bits<16>>,
+//!#      pub write: Signal<In, Bits<16>>,
+//!#      pub full: Signal<Out, Bit>,
+//!#      pub overflow: Signal<Out, Bit>,
+//!#  }
+//!#
+//!#  struct DataWidget {
+//!#      pub data_to_fifo: Signal<Out, Bits<16>>,
+//!#      pub write: Signal<Out, Bits<16>>,
+//!#      pub full: Signal<In, Bit>,
+//!#      pub overflow: Signal<In, Bit>,
+//!#  }
+//!#
+//!#  struct Foo {
+//!#     producer: DataWidget,
+//!#     consumer: MyFIFO,
+//!#  }
+//!impl Logic for Foo {
+//!   #[hdl_gen]
+//!   fn update(&mut self) {
+//!      self.consumer.data_to_fifo.next = self.producer.data_to_fifo.val();
+//!      self.consumer.write.next = self.producer.write.val();
+//!      self.producer.full.next = self.consumer.full.val();
+//!      self.producer.overflow.next = self.consumer.overflow.val();
+//!   }
+//!}
+//!```
 //! This is basically boilerplate at this point, and typing that in and getting it right
 //! is error prone and tedious.  Fortunately, RustHDL can help!  RustHDL includes the
 //! concept of an `Interface`, which is basically a bus.  An `Interface` is generally a
@@ -1110,20 +1176,15 @@
 //! struct MyFIFO {
 //!     // The write interface to the FIFO - now only one line!
 //!     pub write_bus: MyFIFOWriteReceiver,
-//!     // The read interface to the FIFO - unchanged and verbose!
-//!     pub data_from_fifo: Signal<Out, Bits<16>>,
-//!     pub read: Signal<In, Bit>,
-//!     pub empty: Signal<Out, Bit>,
-//!     pub underflow: Signal<Out, Bit>
 //! }
 //!
 //! struct DataWidget {
+//!     // The output interface from the DataWidget!
 //!     pub data_out: MyFIFOWriteSender,
 //! }
 //! ```
 //!
-//! That is significantly less verbose!  With a similar pair for `MyFIFOReadSender/MyFIFOReadReceiver`
-//! you could get to something even shorter.  But leaving it as is for now, what happens to our
+//! That is significantly less verbose!  So what happens to our
 //! `impl Logic for Foo`?  Well, RustHDL autogenerates 2 methods for each `LogicInterface`.  The first
 //! one is called `join`.  And it, well, joins the interfaces.
 //!
@@ -1148,11 +1209,6 @@
 //!#  struct MyFIFO {
 //!#      // The write interface to the FIFO - now only one line!
 //!#      pub write_bus: MyFIFOWriteReceiver,
-//!#      // The read interface to the FIFO - unchanged and verbose!
-//!#      pub data_from_fifo: Signal<Out, Bits<16>>,
-//!#      pub read: Signal<In, Bit>,
-//!#      pub empty: Signal<Out, Bit>,
-//!#      pub underflow: Signal<Out, Bit>
 //!#  }
 //!#  struct DataWidget {
 //!#      pub data_out: MyFIFOWriteSender,
@@ -1560,10 +1616,117 @@
 //! ```
 //! The result of that simulation is here.
 //! ![my_adder_sim](https://github.com/SmithsDigitalForge/rust-hdl/raw/main/rust-hdl/images/my_adder.svg)
+//! Note that the digital flip flop copies it's input from `d` to `q` on the leading edge of the clock.
 //!
 //! ## Generating Verilog
 //!
+//! At some point, you will want to generate Verilog so you can send your design to other
+//! tools.  This is pretty simple.  You call [generate_verilog] and pass it a reference
+//! to the circuit in question.  The [generate_verilog] function will check your circuit,
+//! and then return a string that contains the Verilog equivalent.  It's pretty simple.
+//!
+//! Here is an example.  We will reuse the `MyAdder` circuit from the testbench section,
+//! but this time, generate the Verilog for the circuit instead of simulating it.
+//!
+//! ```rust
+//! use rust_hdl::prelude::*;   // <- shorthand to bring in all definitions
+//!
+//! //        v--- Required by RustHDL
+//! #[derive(LogicBlock, Default)]
+//! struct MyAdder {
+//!     pub sig_a: Signal<In, Bits<8>>,
+//!     pub sig_b: Signal<In, Bits<8>>,
+//!     pub sig_sum: Signal<Out, Bits<8>>,
+//!     pub clock: Signal<In, Clock>,
+//!     my_reg: DFF<Bits<8>>,
+//! }
+//!
+//! impl Logic for MyAdder {
+//!   #[hdl_gen]  // <--- don't forget this
+//!   fn update(&mut self) {
+//!        // Setup the DFF.. this macro is handy to prevent latches
+//!        dff_setup!(self, clock, my_reg);
+//!        self.my_reg.d.next = self.sig_a.val() + self.sig_b.val();
+//!        self.sig_sum.next = self.my_reg.q.val();
+//!    }
+//! }
+//!
+//! let mut uut = MyAdder::default();
+//! uut.connect_all();
+//! println!("{}", generate_verilog(&uut));
+//! ```
+//!
+//! You should get the following generated code in your console:
+//! ```verilog
+//! module top(sig_a,sig_b,sig_sum,clock);
+//!
+//!     // Module arguments
+//!     input wire  [7:0] sig_a;
+//!     input wire  [7:0] sig_b;
+//!     output reg  [7:0] sig_sum;
+//!     input wire  clock;
+//!
+//!     // Stub signals
+//!     reg  [7:0] my_reg$d;
+//!     wire  [7:0] my_reg$q;
+//!     reg  my_reg$clock;
+//!
+//!     // Sub module instances
+//!     top$my_reg my_reg(
+//!         .d(my_reg$d),
+//!         .q(my_reg$q),
+//!         .clock(my_reg$clock)
+//!     );
+//!
+//!     // Update code
+//!     always @(*) begin
+//!         my_reg$clock = clock;
+//!         my_reg$d = my_reg$q;
+//!         my_reg$d = sig_a + sig_b;
+//!         sig_sum = my_reg$q;
+//!     end
+//!
+//! endmodule // top
+//!
+//!
+//! module top$my_reg(d,q,clock);
+//!
+//!     // Module arguments
+//!     input wire  [7:0] d;
+//!     output reg  [7:0] q;
+//!     input wire  clock;
+//!
+//!     // Update code (custom)
+//!     initial begin
+//!        q = 8'h0;
+//!     end
+//!
+//!     always @(posedge clock) begin
+//!        q <= d;
+//!     end
+//!
+//! endmodule // top$my_reg
+//! ```
+//!
+//! A few things about the Verilog generated.
+//!   - It is hierarchical (scoped) by design.  The scopes mimic the scopes inside the RustHDL circuit.
+//!  That makes it easy to map the Verilog back to the RustHDL code if needed when debugging.
+//!   - The code is readable and formatted.
+//!   - The names correspond to the names in RustHDL, which makes it easy to see the details of the logic.
+//!   - RustHDL (at least for this trivial example) is a pretty thin wrapper around Verilog.  That's
+//! good for compatibility with tooling.
+//!
+//! While most FPGAs will require you to use a proprietary and closed source toolchain to synthesize
+//! your design, you can use the open source [Yosys] compiler (if you have it installed) to
+//! check your designs.  For that, you can use the [yosys_validate] function, which runs the Verilog
+//! through some checks and reports on potential errors.  At the moment, [Yosys] is far more
+//! thorough in it's checking than RustHDL, so I highly recommend you install it and use the
+//! [yosys_validate] function on your generated Verilog.
+//!
 //! ## Struct valued signals
+//!
+//! We have seen how Enums and Interfaces can help make your code more compact and readable.
+//!
 //!
 //! ## Loops and Arrays
 //!
@@ -1783,4 +1946,33 @@ fn doc_vlog() {
     uut.connect_all();
     let vlog = generate_verilog(&uut);
     println!("{}", vlog);
+}
+
+#[test]
+fn doc_verilog_demo() {
+    use crate::prelude::*; // <- shorthand to bring in all definitions
+
+    //        v--- Required by RustHDL
+    #[derive(LogicBlock, Default)]
+    struct MyAdder {
+        pub sig_a: Signal<In, Bits<8>>,
+        pub sig_b: Signal<In, Bits<8>>,
+        pub sig_sum: Signal<Out, Bits<8>>,
+        pub clock: Signal<In, Clock>,
+        my_reg: DFF<Bits<8>>,
+    }
+
+    impl Logic for MyAdder {
+        #[hdl_gen] // <--- don't forget this
+        fn update(&mut self) {
+            // Setup the DFF.. this macro is handy to prevent latches
+            dff_setup!(self, clock, my_reg);
+            self.my_reg.d.next = self.sig_a.val() + self.sig_b.val();
+            self.sig_sum.next = self.my_reg.q.val();
+        }
+    }
+
+    let mut uut = MyAdder::default();
+    uut.connect_all();
+    println!("{}", generate_verilog(&uut));
 }
--- rust-hdl/src/prelude.rs
+++ rust-hdl/src/prelude.rs
@@ -1,4 +1,5 @@
 pub use crate::core::prelude::*;
+pub use crate::docs::vcd2svg::vcd_to_svg;
 pub use crate::hls::prelude::*;
 pub use crate::sim::prelude::*;
 pub use crate::widgets::prelude::*;
