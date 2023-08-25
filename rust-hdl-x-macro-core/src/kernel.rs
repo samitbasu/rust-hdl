b356f01e781ee7fa29dda82948d3e3f50a9c3698
Samit Basu <basu.samit@gmail.com>
2023-08-24 17:14:28 -0700

    Add basic AST and start on the proc macro.

---
+++ rust-hdl-x-macro-core/src/kernel.rs
@@ -0,0 +1,223 @@
+use quote::quote;
+use syn::spanned::Spanned;
+type TS = proc_macro2::TokenStream;
+type Result<T> = syn::Result<T>;
+
+pub fn hdl_block(block: &syn::Block) -> Result<TS> {
+    let stmts = block.stmts.iter().map(stmt).collect::<Result<Vec<_>>>()?;
+    Ok(quote! {
+        vec![#(#stmts),*]
+    })
+}
+
+fn stmt(statement: &syn::Stmt) -> Result<TS> {
+    match statement {
+        syn::Stmt::Local(local) => stmt_local(local),
+        syn::Stmt::Expr(expr, semi) => {
+            let expr = hdl_expr(expr)?;
+            if semi.is_some() {
+                Ok(quote! {
+                    rust_hdl_x::ast::Stmt::Semi(#expr)
+                })
+            } else {
+                Ok(quote! {
+                    rust_hdl_x::ast::Stmt::Expr(#expr)
+                })
+            }
+        }
+        _ => Err(syn::Error::new(
+            statement.span(),
+            "Unsupported statement type",
+        )),
+    }
+}
+
+fn stmt_local(local: &syn::Local) -> Result<TS> {
+    let pattern = local_pattern(&local.pat)?;
+    let local_init = local
+        .init
+        .as_ref()
+        .map(|x| hdl_expr(&x.expr))
+        .ok_or(syn::Error::new(
+            local.span(),
+            "Unsupported local declaration",
+        ))??;
+    Ok(quote! {
+        rust_hdl_x::ast::Stmt::Local(Local{pattern: #pattern, value: Box::new(#local_init)})
+    })
+}
+
+fn local_pattern(pat: &syn::Pat) -> Result<TS> {
+    match pat {
+        syn::Pat::Ident(ident) => {
+            let name = &ident.ident;
+            let mutability = ident.mutability.is_some();
+            if ident.by_ref.is_some() {
+                return Err(syn::Error::new(
+                    ident.span(),
+                    "Unsupported reference pattern",
+                ));
+            }
+            Ok(quote! {
+                rust_hdl_x::ast::LocalPattern::Ident(
+                    rust_hdl_x::ast::LocalIdent{
+                        name: stringify!(#name).to_string(),
+                        mutable: #mutability
+                    }
+                )
+            })
+        }
+        syn::Pat::TupleStruct(tuple) => {
+            let elems = tuple
+                .elems
+                .iter()
+                .map(local_pattern)
+                .collect::<Result<Vec<_>>>()?;
+            Ok(quote! {
+                rust_hdl_x::ast::LocalPattern::Tuple(vec![#(#elems),*])
+            })
+        }
+        _ => Err(syn::Error::new(pat.span(), "Unsupported pattern type")),
+    }
+}
+
+fn hdl_expr(expr: &syn::Expr) -> Result<TS> {
+    match expr {
+        syn::Expr::Lit(lit) => hdl_lit(lit),
+        syn::Expr::Binary(binary) => hdl_binary(binary),
+        syn::Expr::Unary(unary) => hdl_unary(unary),
+        syn::Expr::Paren(paren) => hdl_expr(&paren.expr),
+        syn::Expr::Assign(assign) => hdl_assign(assign),
+        syn::Expr::Path(path) => hdl_path(path),
+        _ => Err(syn::Error::new(expr.span(), "Unsupported expression type")),
+    }
+}
+
+fn hdl_path(path: &syn::ExprPath) -> Result<TS> {
+    let ident = path
+        .path
+        .get_ident()
+        .ok_or(syn::Error::new(path.span(), "Unsupported path expression"))?;
+    Ok(quote! {
+        rust_hdl_x::ast::Expr::Ident(stringify!(#ident).to_string())
+    })
+}
+
+fn hdl_assign(assign: &syn::ExprAssign) -> Result<TS> {
+    let left = hdl_expr(&assign.left)?;
+    let right = hdl_expr(&assign.right)?;
+    Ok(quote! {
+        rust_hdl_x::ast::Expr::Assign(Box::new(#left), Box::new(#right))
+    })
+}
+
+fn hdl_unary(unary: &syn::ExprUnary) -> Result<TS> {
+    let op = match unary.op {
+        syn::UnOp::Neg(_) => quote!(rust_hdl_x::ast::UnOp::Neg),
+        syn::UnOp::Not(_) => quote!(rust_hdl_x::ast::UnOp::Not),
+        _ => return Err(syn::Error::new(unary.span(), "Unsupported unary operator")),
+    };
+    let expr = hdl_expr(&unary.expr)?;
+    Ok(quote! {
+        rust_hdl_x::ast::Expr::Unary(
+            rust_hdl_x::ast::ExprUnary
+            {
+                op: #op,
+                expr: Box::new(#expr)
+            }
+        )
+    })
+}
+
+fn hdl_binary(binary: &syn::ExprBinary) -> Result<TS> {
+    let op = match binary.op {
+        syn::BinOp::Add(_) => quote!(rust_hdl_x::ast::BinOp::Add),
+        syn::BinOp::Sub(_) => quote!(rust_hdl_x::ast::BinOp::Sub),
+        syn::BinOp::Mul(_) => quote!(rust_hdl_x::ast::BinOp::Mul),
+        syn::BinOp::And(_) => quote!(rust_hdl_x::ast::BinOp::And),
+        syn::BinOp::Or(_) => quote!(rust_hdl_x::ast::BinOp::Or),
+        syn::BinOp::BitXor(_) => quote!(rust_hdl_x::ast::BinOp::BitXor),
+        syn::BinOp::BitAnd(_) => quote!(rust_hdl_x::ast::BinOp::BitAnd),
+        syn::BinOp::BitOr(_) => quote!(rust_hdl_x::ast::BinOp::BitOr),
+        syn::BinOp::Shl(_) => quote!(rust_hdl_x::ast::BinOp::Shl),
+        syn::BinOp::Shr(_) => quote!(rust_hdl_x::ast::BinOp::Shr),
+        syn::BinOp::Eq(_) => quote!(rust_hdl_x::ast::BinOp::Eq),
+        syn::BinOp::Lt(_) => quote!(rust_hdl_x::ast::BinOp::Lt),
+        syn::BinOp::Le(_) => quote!(rust_hdl_x::ast::BinOp::Le),
+        syn::BinOp::Ne(_) => quote!(rust_hdl_x::ast::BinOp::Ne),
+        syn::BinOp::Ge(_) => quote!(rust_hdl_x::ast::BinOp::Ge),
+        syn::BinOp::Gt(_) => quote!(rust_hdl_x::ast::BinOp::Gt),
+        syn::BinOp::AddAssign(_) => quote!(rust_hdl_x::ast::BinOp::AddAssign),
+        syn::BinOp::SubAssign(_) => quote!(rust_hdl_x::ast::BinOp::SubAssign),
+        syn::BinOp::MulAssign(_) => quote!(rust_hdl_x::ast::BinOp::MulAssign),
+        syn::BinOp::BitXorAssign(_) => quote!(rust_hdl_x::ast::BinOp::BitXorAssign),
+        syn::BinOp::BitAndAssign(_) => quote!(rust_hdl_x::ast::BinOp::BitAndAssign),
+        syn::BinOp::BitOrAssign(_) => quote!(rust_hdl_x::ast::BinOp::BitOrAssign),
+        syn::BinOp::ShlAssign(_) => quote!(rust_hdl_x::ast::BinOp::ShlAssign),
+        syn::BinOp::ShrAssign(_) => quote!(rust_hdl_x::ast::BinOp::ShrAssign),
+        _ => {
+            return Err(syn::Error::new(
+                binary.span(),
+                "Unsupported binary operator",
+            ))
+        }
+    };
+    let left = hdl_expr(&binary.left)?;
+    let right = hdl_expr(&binary.right)?;
+    Ok(quote! {
+        rust_hdl_x::ast::Expr::Binary(
+            rust_hdl_x::ast::ExprBinary {
+                op: #op,
+                lhs: Box::new(#left),
+                rhs: Box::new(#right),
+            }
+        )
+    })
+}
+
+fn hdl_lit(lit: &syn::ExprLit) -> Result<TS> {
+    let lit = &lit.lit;
+    match lit {
+        syn::Lit::Int(int) => {
+            let value = int.base10_digits();
+            Ok(quote! {
+                rust_hdl_x::ast::Expr::Lit(
+                    rust_hdl_x::ast::ExprLit::Int(#value.to_string())
+                )
+            })
+        }
+        syn::Lit::Bool(boolean) => {
+            let value = boolean.value;
+            Ok(quote! {
+                rust_hdl_x::ast::Expr::Lit(
+                    rust_hdl_x::ast::ExprLit::Bool(#value)
+                )
+            })
+        }
+        _ => Err(syn::Error::new(lit.span(), "Unsupported literal type")),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_basic_block() {
+        let test_code = quote! {
+            {
+                let a = 1;
+                let b = 2;
+                let c = a + b;
+                let mut d = 3;
+                c
+            }
+        };
+        let block = syn::parse2::<syn::Block>(test_code).unwrap();
+        let result = hdl_block(&block).unwrap();
+        let result = format!("fn jnk() -> Vec<Stmt> {{ {} }}", result);
+        let result = result.replace("rust_hdl_x :: ast :: ", "");
+        let result = prettyplease::unparse(&syn::parse_file(&result).unwrap());
+        println!("{}", result);
+    }
+}
--- rust-hdl-x/src/ast.rs
+++ rust-hdl-x/src/ast.rs
@@ -1,32 +1,29 @@
-pub struct Block {
-    pub statements: Vec<Stmt>,
-}
-
+#[derive(Debug)]
 pub enum Stmt {
     Local(Local),
-    Item(Item),
     Expr(Expr),
+    Semi(Expr),
 }
 
+#[derive(Debug)]
 pub struct Local {
     pub pattern: LocalPattern,
     pub value: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub enum LocalPattern {
-    Ident(String),
+    Ident(LocalIdent),
     Tuple(Vec<LocalPattern>),
 }
 
-pub enum Item {
-    Const(ItemConst),
-}
-
-pub struct ItemConst {
+#[derive(Debug)]
+pub struct LocalIdent {
     pub name: String,
-    pub value: Box<Expr>,
+    pub mutable: bool,
 }
 
+#[derive(Debug)]
 pub enum Expr {
     Binary(ExprBinary),
     Unary(ExprUnary),
@@ -41,40 +38,43 @@ pub enum Expr {
     Assign(ExprAssign),
     Group(Box<Expr>),
     Field(ExprField),
-    Block(ExprBlock),
+    Block(Vec<Stmt>),
     Array(ExprArray),
     Range(ExprRange),
+    Ident(String),
 }
 
+#[derive(Debug)]
 pub struct ExprArray {
     pub elems: Vec<Expr>,
 }
 
-pub struct ExprBlock {
-    pub block: Block,
-}
-
+#[derive(Debug)]
 pub struct ExprField {
     pub base: Box<Expr>,
     pub member: Member,
 }
 
+#[derive(Debug)]
 pub enum Member {
     Named(String),
     Unnamed(u32),
 }
 
+#[derive(Debug)]
 pub struct ExprAssign {
     pub lhs: Box<Expr>,
     pub rhs: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub struct ExprBinary {
     pub op: BinOp,
     pub lhs: Box<Expr>,
     pub rhs: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub enum BinOp {
     Add,
     Sub,
@@ -102,62 +102,103 @@ pub enum BinOp {
     ShrAssign,
 }
 
+#[derive(Debug)]
 pub struct ExprUnary {
     pub op: UnOp,
     pub expr: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub enum UnOp {
     Neg,
     Not,
 }
 
+#[derive(Debug)]
 pub struct ExprIf {
     pub cond: Box<Expr>,
-    pub then_branch: Block,
+    pub then_branch: Vec<Stmt>,
     pub else_branch: Option<Box<Expr>>,
 }
 
+#[derive(Debug)]
 pub struct ExprMatch {
     pub expr: Box<Expr>,
     pub arms: Vec<Arm>,
 }
 
+#[derive(Debug)]
 pub struct Arm {
     pub pattern: Pattern,
     pub guard: Option<Box<Expr>>,
     pub body: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub enum Pattern {
     Lit(ExprLit),
     Or(Vec<Pattern>),
     Paren(Box<Pattern>),
 }
 
+#[derive(Debug)]
 pub enum ExprLit {
     Int(String),
     Bool(bool),
 }
 
+#[derive(Debug)]
 pub struct ExprIndex {
     pub expr: Box<Expr>,
     pub index: Box<Expr>,
 }
 
+#[derive(Debug)]
 pub struct ExprForLoop {
     pub pat: Box<LocalPattern>,
     pub expr: Box<Expr>,
-    pub body: Block,
+    pub body: Vec<Stmt>,
 }
 
+#[derive(Debug)]
 pub struct ExprRange {
     pub start: Option<Box<Expr>>,
     pub limits: RangeLimits,
     pub end: Option<Box<Expr>>,
 }
 
+#[derive(Debug)]
 pub enum RangeLimits {
     HalfOpen,
     Closed,
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_fn() {
+        fn jnk() -> Vec<Stmt> {
+            vec![
+                Stmt::Local(Local {
+                    pattern: LocalPattern::Ident(stringify!(a).to_string()),
+                    value: Box::new(Expr::Lit(ExprLit::Int("1".to_string()))),
+                }),
+                Stmt::Local(Local {
+                    pattern: LocalPattern::Ident(stringify!(b).to_string()),
+                    value: Box::new(Expr::Lit(ExprLit::Int("2".to_string()))),
+                }),
+                Stmt::Local(Local {
+                    pattern: LocalPattern::Ident(stringify!(c).to_string()),
+                    value: Box::new(Expr::Binary(ExprBinary {
+                        op: BinOp::Add,
+                        lhs: Box::new(Expr::Ident(stringify!(a).to_string())),
+                        rhs: Box::new(Expr::Ident(stringify!(b).to_string())),
+                    })),
+                }),
+                Stmt::Expr(Expr::Ident(stringify!(c).to_string())),
+            ]
+        }
+        println!("{:#?}", jnk());
+    }
 }
